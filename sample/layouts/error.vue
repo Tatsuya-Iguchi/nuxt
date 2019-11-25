1903f3def76c266290e71e467a7eb5f008f11ac1
Tatsuya-Iguchi <tatsu.smartphone@gmail.com>
2019-11-25 14:43:33 +0900

    test: jest 追加

--- sample/layouts/error.vue
+++ sample/layouts/error.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <h1 v-if="error.statusCode === 404">ページが見つかりません</h1>
     <h1 v-else>エラーが発生しました</h1>
-    <nuxt-link to="/">ホーム</nuxt-link>
+    <div class="links">
+      <nuxt-link to="/">ホーム</nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -12,3 +14,7 @@
     layout: 'blog' // エラーページ用のカスタムレイアウトを指定できます
   }
 </script>
+
+<style>
+
+</style>
---
+++ sample/src/todos/todo.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+
+
+describe('hogehoge', () => {
+  it('fugafuga', () => {
+    const abc = true
+
+    expect(abc).toBeTruthy()
+  })
+})
